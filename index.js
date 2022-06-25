02af087d3a69e0e197b58945cbda9b83f6495fbe
Junaid-eko Raimot <99554053+junaideko@users.noreply.github.com>
2022-06-25 16:57:00 +0000

    made changes

--- index.js
+++ index.js
@@ -6,9 +6,43 @@ const routes = require("./routes/flightRoute");
 
 const app = express();
 
-app.use(json());
+const {
+   ServerResponse
+} = require("http");
+const http = require("http").createServer(app);
+const flights = require("./controllers/flightController");
+const models = require("./models/flight");
+const routes = require("./routes/flightRoute");
+const port = process.env.PORT || 3000;
+ServerResponse
+
+
+//add/book flight
+const flightDetails = {
+   title: 'flight to canada',
+   time: '1pm',
+   price: 26000,
+   date: '26-06-2022'
+}
+
+function flightdata() {
+   return flightDetails;
+};
+flightdata();
+
+app.use(express.json());
 
 app.use("/", routes);
+
+app.use("/", flights);
+
+app.use("/", models)
+
+app.get("/", (req, res) => {
+  res.json('Hello World')
+})
+
+
 
 const port = process.env.PORT || 3000;
 
